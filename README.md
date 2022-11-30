6ce46aa136099005156a645ad1a292866aede845
nhz2 <nhz2@cornell.edu>
2022-11-30 11:41:30 -0500

    docs

--- README.md
+++ README.md
@@ -1,7 +1,90 @@
-# StorageTrees
+# StorageTrees (WIP)
 
 [![Build Status](https://github.com/medyan-dev/StorageTrees.jl/workflows/CI/badge.svg)](https://github.com/medyan-dev/StorageTrees.jl/actions)
 
 
 
-Hierarchy of arrays and attributes loaded from disk or to be saved to disk with Zarr.
+In memory hierarchy of arrays and attributes loaded from disk or to be saved to disk with Zarr.
+
+## Warning: StorageTrees is currently a WIP and its API may drastically change at any time.
+
+If you need to store huge datasets that cannot fit uncompressed in memory consider using https://github.com/JuliaIO/HDF5.jl or https://github.com/JuliaIO/Zarr.jl
+
+If you just want to serialize arbitrary Julia data consider using https://github.com/JuliaIO/JLD2.jl or https://github.com/invenia/JLSO.jl
+
+## Goals
+
+1. `ZGroup` represents a tree with arrays as leaves.
+1. `ZGroup` leaf arrays are uncompressed but store metadata about how they should be losslessly compressed when saved to disk.
+1. `ZGroup` can have JSON3 serializable attributes attached to any node or leaf.
+1. Data can be quickly accessed and modified in `ZGroup`.
+1. No file open close semantics. Use the Julia garbage collector to clean memory up.
+1. Save and load `ZGroup` in a directory in Zarr format.
+1. `ZGroup` saved to a directory can be read in other languages.
+
+
+## Examples
+
+Create an empty `ZGroup` with:
+
+```julia
+using StorageTrees
+zg = ZGroup()
+```
+
+Save and copy data to the group with:
+
+```julia
+zg["random_data"] = rand(30,10)
+```
+
+Save and copy data a sub group using "/" as a path separator.
+
+All intermediate groups will be automatically created.
+
+```julia
+zg["group1/subgroup2/random_data"] = rand(30)
+zg
+```
+```
+📂 
+├─ "group1" ⇒ 📂 
+│             └─ "subgroup2" ⇒ 📂 
+│                              └─ "random_data" ⇒ 🔢 30 Float64 
+└─ "random_data" ⇒ 🔢 30×10 Float64 
+```
+
+Sub groups can also be added to a group.
+Note unlike `setindex!` with an `AbstractArray`, this doesn't make a copy of the added group, it is just a reference.
+```julia
+othergroup = ZGroup()
+othergroup["bar"] = rand(Int,3)
+zg["innergroup2"] = othergroup
+zg
+```
+```
+📂 
+├─ "group1" ⇒ 📂 
+│             └─ "subgroup2" ⇒ 📂 
+│                              └─ "random_data" ⇒ 🔢 30 Float64 
+├─ "innergroup2" ⇒ 📂 
+│                  └─ "bar" ⇒ 🔢 3 Int64 
+└─ "random_data" ⇒ 🔢 30×10 Float64 
+```
+
+
+Arrays can be read with collect. This returns a copy of the array.
+
+```julia-repl
+julia> collect(zg["innergroup2/bar"])
+3-element Vector{Int64}:
+  8547501744104122400
+ -1824477752480561305
+  6628972061588725027
+
+julia> collect(Int128,zg["innergroup2/bar"])
+3-element Vector{Int128}:
+  8547501744104122400
+ -1824477752480561305
+  6628972061588725027
+```
---
+++ src/ZGroup.jl
@@ -0,0 +1,103 @@
+Base.@kwdef struct ZGroup
+    children::SortedDict{String,Union{ZArray,ZGroup}} = SortedDict{String,Union{ZArray,ZGroup}}()
+    attrs::OrderedDict{String,Any} = OrderedDict{String,Any}()
+end
+
+"""
+Return the mutable SortedDict of attributes.
+"""
+attrs(d::ZGroup) = d.attrs
+
+AbstractTrees.children(d::ZGroup) = d.children
+
+AbstractTrees.childrentype(::Type{ZGroup}) = SortedDict{String,Union{ZArray,ZGroup}}
+
+AbstractTrees.childtype(::Type{ZGroup}) = Union{ZArray,ZGroup}
+
+function _normalize_path(pathstr::AbstractString)::Vector{SubString{String}}
+    path = split(replace(pathstr, '\\'=>'/'), '/'; keepempty=false)
+    @argcheck !any(==("."), path)
+    @argcheck !any(==(".."), path)
+    path
+end
+
+function Base.getindex(d::ZGroup, pathstr::AbstractString)
+    path = _normalize_path(pathstr)
+    foldl((x,y)->getindex(x.children, y), path; init=d)
+end
+
+"""
+Make all groups in path if they don't already exist.
+Return the last group.
+"""
+function makegroups(d::ZGroup, path::Vector{<:AbstractString})
+    gr::ZGroup = d
+    for part in path
+        if !haskey(gr.children, part)
+            #create path if it doesn't exist
+            gr.children[part] = ZGroup()
+        end
+        gr = gr.children[part]
+    end
+    gr
+end
+
+function Base.setindex!(d::ZGroup, x::Union{ZGroup,ZArray}, pathstr::AbstractString)
+    path = _normalize_path(pathstr)
+    lastgroup = makegroups(d, path[begin:end-1])
+    setindex!(lastgroup.children, x, path[end])
+    d
+end
+
+function Base.setindex!(d::ZGroup, x::AbstractArray, pathstr::AbstractString)
+    setindex!(d, ZArray(collect(x)), pathstr)
+end
+
+Base.keys(d::ZGroup) = keys(children(d))
+
+function Base.haskey(d::ZGroup, pathstr::AbstractString)
+    path = _normalize_path(pathstr)
+    gr::ZGroup = d
+    pathexists = true
+    for i in 1:length(path)-1
+        part = String(path[i])
+        if haskey(gr.children, part)
+            child = gr.children[part]
+            if child isa ZGroup
+                gr = child
+            else
+                pathexists = false
+                break
+            end
+        else
+            pathexists = false
+            break
+        end
+    end
+    pathexists && haskey(gr.children, path[end])
+end
+
+Base.values(d::ZGroup) = values(children(d))
+
+Base.pairs(d::ZGroup) = pairs(children(d))
+
+
+function Base.delete!(d::ZGroup, pathstr::AbstractString)
+    path = _normalize_path(pathstr)
+    lastgroup::ZGroup = foldl((x,y)->getindex(x.children, y), path[begin:end-1]; init=d)
+    delete!(lastgroup.children, path[end])
+end
+
+
+function Base.show(io::IO, ::MIME"text/plain", d::ZGroup)
+    print_tree(io, d; maxdepth=5, printkeys=true) do io, node
+        if node isa ZGroup
+            print(io, "📂 ")
+        elseif node isa ZArray
+            print(io, "🔢 ", join(size(getarray(node)),"×"), " ", eltype(getarray(node)), " ")
+        end
+        for (k, v) in attrs(node)
+            print(io, "🏷️\"$k\" => $(repr(v)), ")
+        end
+    end
+end
