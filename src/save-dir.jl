c37dcf6384bf0ec8c271ea5608aca1ca3275e1c6
nhz2 <nhz2@cornell.edu>
2022-12-09 17:30:53 -0500

    reading fixture

--- src/save-dir.jl
+++ src/save-dir.jl
@@ -95,6 +95,7 @@ function load_zgroup(dirpath, subpath::String)::ZGroup
         child_subpath = String(subpath*k*"/")
         out[k] = load_zgroup(dirpath, child_subpath)
     end
+    @show subpath
     subarray_keys::Vector{String} = sort(string.(collect(zg.array_keys())))
     for k in subarray_keys
         child_subpath = String(subpath*k*"/")
@@ -109,9 +110,21 @@ function load_zarray(dirpath, subpath::String)::ZArray
         path=subpath,
         mode="r",
     )
-    data = Array(PyArray(za.get_basic_selection()))
+    data, chunks = try
+        Array(PyArray(za.get_basic_selection())), Tuple(pyconvert(Vector{Int},za.chunks))
+    catch e
+        if endswith(e.msg, "cannot convert this Python 'ndarray' to a 'PyArray'")
+            Array(PyArray(za.get_basic_selection().tobytes())), 0
+        else
+            rethrow()
+        end
+    end
+    if !(eltype(data) isa ZDataTypes)
+        data = collect(vec(reinterpret(UInt8,data)))
+        chunks = 0
+    end
     ZArray(data;
-        chunks=Tuple(pyconvert(Vector{Int},za.chunks)),
+        chunks,
         attrs=load_attrs(dirpath, subpath),
         compressor="default", # TODO add compressor and filter loading
     )
---
+++ test/test_read-fixture.jl
@@ -0,0 +1,48 @@
+using StorageTrees
+using DataStructures: SortedDict, OrderedDict
+using Test
+using Pkg.Artifacts
+
+using PythonCall
+
+zarr = pyimport("zarr")
+
+
+"""
+Compare the results of reading a directory with zarr-python and StorageTrees.
+
+The directory must be a group, not an array.
+This function is call 
+"""
+function disk_load_compare(zarr, dirpath=joinpath(artifact"fixture","fixture"))
+    # get path of python group
+    python_group = zarr.open_group(
+        store=zarr.DirectoryStore(dirpath),
+        path="/", # Group path within store.
+        mode="r", # Persistence mode: 
+            # ‘r’ means read only (must exist); 
+            # ‘r+’ means read/write (must exist); 
+            # ‘a’ means read/write (create if doesn’t exist); 
+            # ‘w’ means create (overwrite if exists); 
+            # ‘w-’ means create (fail if exists).
+    )
+    zgroup = StorageTrees.load_dir(dirpath)
+    compare_jl_py_groups(zgroup, python_group)
+
+
+end
+
+
+function compare_jl_py_groups(jl_group::ZGroup, py_group)
+    @test PyDict(py_group.attrs.asdict()) == attrs(jl_group)
+    py_subgroup_keys::Vector{String} = sort(string.(collect(py_group.group_keys())))
+    jl_subgroup_keys = map(first ,filter(x->x[2] isa ZGroup, collect(pairs(jl_group))))
+    @test py_subgroup_keys == jl_subgroup_keys
+    for groupkey in py_subgroup_keys
+        compare_jl_py_groups(jl_group[groupkey], py_group[groupkey])
+    end
+end
+
+@testset "read fixture data and compare to zarr-python" begin
+    disk_load_compare(zarr)
+end
